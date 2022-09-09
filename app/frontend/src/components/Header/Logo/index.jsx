b58547575e2caea2a246996e4a892dac47ae929a
Marco <marcoantonio99d@gmail.com>
2022-09-09 01:37:07 -0300

    logo component

---
+++ app/frontend/src/components/Header/Logo/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.css';
+import logo from '../../../images/logo.png';
+
+export default function Logo() {
+  return (
+    <div className={ style.container }>
+      <img className={ style.logo } src={ logo } alt="Logo" />
+      <h1 className={ style.title }>Metronomo Discos.</h1>
+    </div>
+  )
+}
